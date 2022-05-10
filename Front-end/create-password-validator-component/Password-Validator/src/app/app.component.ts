aacb871841e90b3bcdb4206dd73fdc1815131217
Priyanshu Bandyopadhyay <priyanshu.banerjee1311@gmail.com>
2022-05-10 15:28:33 +0530

    commit

---
+++ Front-end/create-password-validator-component/Password-Validator/src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  exform!: FormGroup;
+  ngOnInit(): void {
+    this.exform = new FormGroup({
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+        Validators.maxLength(20),
+      ]),
+    });
+  }
+  clicksub() {
+    console.log(this.exform.value);
+    this.exform.reset();
+  }
+  get name() {
+    return this.exform.get('name');
+  }
+  get email() {
+    return this.exform.get('email');
+  }
+  get phone() {
+    return this.exform.get('password');
+  }
+}
