54ffffd6d16a93238a120a9e67e7d59f8c454102
Priyanshu Bandyopadhyay <priyanshu.banerjee1311@gmail.com>
2022-05-13 12:01:33 +0530

    commit

--- Front-end/viewtube-with-upload-functionality/src/app/app.module.ts
+++ Front-end/viewtube-with-upload-functionality/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { VideoContentComponent } from './video-content/video-content.component';
 import { ChipsBarComponent } from './chips-bar/chips-bar.component';
+import { StudioUploadComponent } from './studio-upload/studio-upload.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     VideoContentComponent,
-    ChipsBarComponent
+    ChipsBarComponent,
+    StudioUploadComponent
   ],
   imports: [
     BrowserModule    
---
+++ Front-end/viewtube-with-upload-functionality/src/app/studio-upload/studio-upload.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-studio-upload',
+  templateUrl: './studio-upload.component.html',
+  styleUrls: ['./studio-upload.component.css']
+})
+export class StudioUploadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
